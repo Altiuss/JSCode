2aeb005495fb16039823ec57eeab75fd7522f3e0
Altiuss <altiuss@inbox.lv>
2022-08-01 12:53:45 +0200

    Added_JS_lesson_1

---
+++ JSCurs/Lesson1/scripts/script.js
@@ -0,0 +1,19 @@
+let number = 5;
+let string = "hallo";
+let  firstname = "Oleg";
+let city = "Berlin";
+
+console.log(' Sveiciens caur konkatenaciju: ' + string + ` ` + firstname); 
+console.log(` Sveiciens caur interpolaciju: ${city} ${string} ${firstname}`);
+
+let a = 5;
+let b = "hello";
+console.log(typeof a);
+console.log(typeof city);
+
+let y = (7 + "7") / 2;
+let o = (10 - "7");
+console.log(y);
+console.log(o);
+
+
