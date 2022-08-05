dd0e30af08d39b1875d6011e2cf47be15eca8c4f
Altiuss <altiuss@inbox.lv>
2022-08-05 13:38:16 +0200

    added konsult1

---
+++ JSCurs/Lesson1Gaik/script/script.js
@@ -0,0 +1,84 @@
+// const age = 29;
+// const name = 'Aigars';
+// const lastName = 'Alutis';
+// const is_teacher = true;
+// const fullName = `${name} ${lastName}`;
+
+// console.log(fullName);
+
+// console.log(age);
+// console.log(name);
+// console.log(is_teacher);
+
+// console.log(typeof(age));
+// console.log(typeof(name));
+// console.log(typeof(is_teacher));
+
+// const red = 134;
+// const green = 124;
+// const blue = 56;
+
+// console.log("rgb(" + red + ", " + green + ", " + blue + ");");
+
+// console.log(`rgb(${red}, ${green}, ${blue});`);
+
+//  const name = 'Aigars';
+//  const lastname = 'Alutis';
+//  const age = 47;
+ 
+//  console.log('Hallo, ' + name + ' ' + lastname + '! You have ' + age * 356 * 24 * 60 + ' minute.');
+
+//  console.log(`Hallo, ${name} ${lastname}! You have  ${ age * 356 * 24 * 60} minute.`);
+
+//  const value_1 = 112;
+//  const value_2 = 25;
+
+//  console.log(value_1 + value_2);
+//  console.log(value_1 - value_2);
+//  console.log(value_1 * value_2);
+//  console.log(value_1 / value_2);
+//  console.log(value_1 % value_2);
+
+
+// const value_1 = '12';
+
+// console.log(value_1);
+
+// let result = ('b' + 'a' + + 'c').toLowerCase();
+// console.log(result);
+
+// const a = +prompt('Uzradiet pirmo skaitli:')
+// const b = +prompt('Uzradiet otro skaitli:')
+
+// const a_number = a;
+// const b_number = a;
+
+
+// console.log(`${a} + ${b} = ${a + b} `);
+// console.log(`${a} - ${b} = ${a - b} `);
+// console.log(`${a} * ${b} = ${a * b} `);
+// console.log(`${a} / ${b} = ${a / b} `);
+
+// const a = prompt('noradiet skaitli:')
+
+// if (a > 0){
+//     console.log(`skaitlis ${a} pozitivs`);
+    
+// } else {
+//     console.log(`skaitlis ${a} negativs`);
+// }
+
+
+const a = +prompt('noradiet skaitli:');
+const b = +prompt('noradiet skaitli:');
+
+if (a > b){
+    console.log(`${a} bolse ${b} na ${a - b}`);
+} else if (a < b){
+    console.log(`${a} mense ${b} na ${b - a}`);
+
+    } else {
+        console.log('skaitli ir vienadi!')
+    }
+    
+
