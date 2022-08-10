6364f22c154b99f741f0865748f9efafd10d4a0d
Altiuss <altiuss@inbox.lv>
2022-08-10 10:37:07 +0200

    Ended lesson3

--- JSCurs/Lesson3/scripts/functions.js
+++ JSCurs/Lesson3/scripts/functions.js
@@ -22,8 +22,30 @@
 // }
 // console.log(multiplicateNumber(3, 5));
 
-function returnString(firstName = "Elena", age = 18) {
-  return `Hello, ${firstName}. Your age is ${age}`;
+// function returnString(firstName = "Elena", age = 18) {
+//   return `Hello, ${firstName}. Your age is ${age}`;
+// }
+
+// console.log(returnString(undefined, 145));
+
+console.log(greeting_declaration("Olga", "Petrova"));
+
+function greeting_declaration(firstName, lastName) {
+  return `Hello ${firstName} ${lastName}`;
 }
 
-console.log(returnString(undefined, 145));
+// Funkcioalie izteicieni
+
+let greeting = function getHello(firstName, lastName) {
+  return `Hello ${firstName} ${lastName}`;
+};
+
+console.log(greeting("Olga", "Petrova"));
+
+// Strelocnie funkciji
+
+let greeting_arrow = (firstName, lastName) => {
+  return `Hello ${firstName} ${lastName}`;
+};
+
+console.log(greeting_arrow("Olga", "Petrova"));
