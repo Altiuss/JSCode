0b764b472f8dbdb3e137964359172466622f5bfc
Altiuss <altiuss@inbox.lv>
2022-11-13 22:39:38 +0100

    added changes

--- JSCurs/Lesson 4 React SaitTemplate/site_template/src/components/Contacts/index.jsx
+++ JSCurs/Lesson 4 React SaitTemplate/site_template/src/components/Contacts/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ContactsForm from "../ContactsForm";
 import s from "./index.module.css";
+import { MailOutlined } from "@ant-design/icons";
 
 export default function Contacts() {
   return (
@@ -13,7 +14,9 @@ export default function Contacts() {
           </p>
           <div className={s.phone_and_mail}>
             <p>+7 (111) 222-33-44</p>
-            <p>order@bu-one.ru</p>
+            <p>
+              <MailOutlined /> order@bu-one.ru
+            </p>
           </div>
         </div>
         <ContactsForm />
--- JSCurs/Lesson 4 React SaitTemplate/site_template/src/components/ContactsForm/index.jsx
+++ JSCurs/Lesson 4 React SaitTemplate/site_template/src/components/ContactsForm/index.jsx
@@ -1,5 +1,33 @@
 import React from "react";
+import s from "./index.module.css";
 
 export default function ContactsForm() {
-  return <div>ContactsForm</div>;
+  return (
+    <div className={s.root}>
+      <div className={s.roof}>
+        <div>
+          <p>Имя</p>
+          <input type="text" placeholder="Иван" />
+          <hr />
+        </div>
+        <div>
+          <p>Фамилия</p>
+          <input type="text" placeholder="Иванов " />
+          <hr />
+        </div>
+      </div>
+      <div className={s.message_div}>
+        <p>Сообщение</p>
+        <textarea
+          name=""
+          placeholder="Ваше сообщение"
+          id=""
+          cols="50"
+          rows="5"
+        ></textarea>
+        <hr />
+      </div>
+      <button>Отправить сообщение</button>
+    </div>
+  );
 }
