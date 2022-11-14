bd65af757e4f9c7afdfddbff7f0f0e570925e5ae
Altiuss <altiuss@inbox.lv>
2022-11-14 10:31:46 +0100

    added Footer

--- JSCurs/Lesson 4 React SaitTemplate/site_template/src/components/App/App.js
+++ JSCurs/Lesson 4 React SaitTemplate/site_template/src/components/App/App.js
@@ -3,6 +3,7 @@ import Nav from "../Nav";
 import Services from "../Services";
 import AbautUs from "../AboutUs";
 import Contacts from "../Contacts";
+import Footer from "../Footer";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Services />
       <AbautUs />
       <Contacts />
+      <Footer />
     </div>
   );
 }
