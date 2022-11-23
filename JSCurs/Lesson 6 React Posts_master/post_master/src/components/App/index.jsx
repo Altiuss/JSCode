7adadbd86b54710e0f0beaf119da1317174bb016
Altiuss <altiuss@inbox.lv>
2022-11-23 12:45:50 +0100

    Ended lesson

--- JSCurs/Lesson 6 React Posts_master/post_master/src/components/App/index.jsx
+++ JSCurs/Lesson 6 React Posts_master/post_master/src/components/App/index.jsx
@@ -2,12 +2,25 @@ import PostContainer from "../PostContainer";
 import { useState } from "react";
 import { posts_data } from "../../data/posts";
 import { Context } from "../../context";
+import AddPostForm from "../AddPostForm";
 
 export default function App() {
   const [posts, setPosts] = useState(posts_data);
 
+  const change_like = (id) => {
+    const target_card = posts.find((el) => el.id === id);
+    target_card.like = !target_card.like;
+
+    setPosts([...posts]);
+  }
+
+  const add_post = {title_value, text_value} => {
+
+  }
+
   return (
-    <Context.Provider value={{ posts }}>
+    <Context.Provider value={{ posts, change_like }}>
+      <AddPostForm />
       <PostContainer />
     </Context.Provider>
   );
--- JSCurs/Lesson 6 React Posts_master/post_master/src/data/posts.js
+++ JSCurs/Lesson 6 React Posts_master/post_master/src/data/posts.js
@@ -4,23 +4,40 @@ export const posts_data = [
     title: "About bikes",
     text: "Detailed information about bikes",
     like: false,
+    comments: [
+      {
+        id: 1,
+        comment: "Good point",
+      },
+      {
+        id: 2,
+        comment: "I disagree",
+      },
+      {
+        id: 3,
+        comment: "Fine",
+      },
+    ],
   },
   {
     id: 2,
     title: "About weather",
     text: "Weather is good today",
-    like: false,
+    like: true,
+    comments: [],
   },
   {
     id: 3,
     title: "About university",
     text: "I hate my classes!",
     like: false,
+    comments: [],
   },
   {
     id: 4,
     title: "About children",
     text: "I love my children",
     like: false,
+    comments: [],
   },
 ];
