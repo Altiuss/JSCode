5213e8dc7a83869a123feeff09a2447ac67dbf17
Altiuss <altiuss@inbox.lv>
2023-02-03 10:38:15 +0100

    make changes in repozitory

--- JSCurs/Lesson14React redux-fetch/redux-fetch/src/requests/product_req.js
+++
@@ -1,9 +0,0 @@
-import { loadProduct } from "../store/reducers/product";
-
-export const load_product = (id) => {
-  return (dispatch) => {
-    fetch(`https://fakestoreapi.com/products/${id}`)
-      .then((resp) => resp.json())
-      .then((json) => dispatch(loadProduct(json)));
-  };
-};
