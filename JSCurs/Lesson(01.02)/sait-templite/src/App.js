f033c8aed6da91cf2ddc24568404b2a93c8b8170
Altiuss <altiuss@inbox.lv>
2023-02-06 13:02:08 +0100

    ended lesson 1.2

--- JSCurs/Lesson(01.02)/sait-templite/src/App.js
+++ JSCurs/Lesson(01.02)/sait-templite/src/App.js
@@ -1,12 +1,16 @@
-import  Header  from "./components/Header";
+import Header from "./components/Header";
 import "./App.css";
 import Banner1 from "./components/Banner1";
+import Products from "./components/Products";
 
 function App() {
-  return <div>
-    <Header />
-    <Banner1/>
-  </div>;
+  return (
+    <div>
+      <Header />
+      <Banner1 />
+      <Products />
+    </div>
+  );
 }
 
 export default App;
---
+++ JSCurs/Lesson(01.02)/sait-templite/src/components/Products/data/products.js
@@ -0,0 +1,35 @@
+import first from '../media/first.png';
+import second from '../media/second.png';
+import third from '../media/third.png';
+import fourth from '../media/fourth.png';
+
+export const products = [
+    {
+      id: 1,
+      title: 'Convert with Automations',
+      image: first,
+      description: 'Send the right message at the right time to convert more customers.',
+      background_color: 'color_1'
+    },
+    {
+      id: 2,
+      title: 'Optimize with AI & Analyticss',
+      image: second,
+      description: 'Get AI-assisted suggestions to help make your content more engaging.',
+      background_color: 'color_2'
+    },
+    {
+      id: 3,
+      title: 'Target with Segmentation',
+      image: third,
+      description: 'Boost customer loyalty by predicting the right ones to target.',
+      background_color: 'color_3'
+    },
+    {
+      id: 4,
+      title: 'Sync with Integrations',
+      image: fourth,
+      description: 'Connect more tools, get more data, save time, and make better decisions.',
+      background_color: 'color_4'
+    }
+  ]
