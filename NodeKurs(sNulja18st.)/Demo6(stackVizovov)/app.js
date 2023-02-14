95c0839eda20a964979d16d6115f0a940daae77b
Altiuss <altiuss@inbox.lv>
2023-02-14 20:53:47 +0100

    add collingStack

---
+++ NodeKurs(sNulja18st.)/Demo6(stackVizovov)/app.js
@@ -0,0 +1,23 @@
+const a = 5;
+
+function b() {
+    return c();
+   
+}
+
+function c() {
+    return d();
+   
+}
+function d() {
+    console.log(a);
+   
+}
+
+setTimeout(() => {
+    console.log("Timeout");
+}, 1000);
+
+
+
+b();
