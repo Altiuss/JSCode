14fe55590df090568d81cbd427857bfcbb3c49d6
Altiuss <altiuss@inbox.lv>
2023-03-05 19:16:53 +0100

    endded Lesson3

--- Bek/Lesson3/routes/auth.js
+++ Bek/Lesson3/routes/auth.js
@@ -1,32 +1,20 @@
 import { Router } from "express";
 import User from "../database/schemas/User.js";
-import { hashPassword } from "../utilits/utils.js";
+import { hashPassword, comparePassword } from "../utilits/utils.js";
 
 const router = Router();
 
 router.post('/login', async (req, res) => {
   const { email, password } = req.body;
-  if(!email || !password) return res.send(400);
+  
+  if(!email || !password) return res.status(400).send('Please provide email and password');
 
-  const userdDB = await User.findOne({ email });
-  if(!userdDB) return res.send(401);
+  const usedDB = await User.findOne({ email });
+  if(!usedDB) return res.status(401).send('User does not exist');
 
-  const isValid = comparePassword(password, userdDB.password);
-  isValid ? res.send(200) : res.send(401);
+  const isPasswordValid = comparePassword(password, usedDB.password);
+  isPasswordValid ? res.send('You are logged in') : res.status(401).send('Invalid password');
 });
-// router.post("/login", (req, res) => {
-//   const { username, password } = req.body;
-//   if (username && password) {
-//     if (req.session.user) {
-//       res.send("You are already logged in");
-//     } else {
-//       req.session.user = { username };
-//       res.send(req.session);
-//     }
-//   } else {
-//     res.send("Please provide username and password");
-//   }
-// });
 
 router.post("/register", async (req, res) => {
   const { email } = req.body;
--- Bek/Lesson3/routes/cart.js
+++ Bek/Lesson3/routes/cart.js
@@ -12,6 +12,7 @@ router.get("/", (req, res) => {
 });
 
 router.post("/item", (req, res) => {
+ 
   const { id, name,  mobile, color, quantity } = req.body;
   const cartItem = { id, name, mobile, color, quantity };
   const { cart } = req.session;
@@ -22,6 +23,7 @@ router.post("/item", (req, res) => {
       items: [cartItem],
     };
   }
+  console.log(req.session);
   console.log(req.sessionID);
   console.log(req.body);
   res.send("Cart is updated");
